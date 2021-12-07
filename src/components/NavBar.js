084cb9a9f345ef74c074eeadb4da79226f9aaba5
Cristian Mellibosky <melliboskycristian@gmail.com>
2021-12-07 14:21:17 -0300

    se agrego navBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+const NavBar = () => {    
+    return ( 
+            <>
+
+            <nav>
+                <div class="nav-wrapper">
+                <a href="#" class="brand-logo">Tienda Musical Cristia Mellibosky</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="sass.html">Instrumentos</a></li>
+                    <li><a href="badges.html">Clases On Line</a></li>
+                    <li><a href="collapsible.html">Contacto</a></li>
+                </ul>
+                </div>
+            </nav>
+            </>
+        )
+}
+
+export default NavBar
